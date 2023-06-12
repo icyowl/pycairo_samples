6e614e5caacf2be163fb728dca80e7dac1b4e466
AkiraMac <frogcherry7@gmail.com>
2023-06-12 18:28:37 +0900

    hey

--- l-system-try.py
+++ l-system-try.py
@@ -8,17 +8,33 @@ def lSysGenerate(s, order):
     return s
 
 def lSysCompute(s):
-    # d = {'F': 'F-F++F-F'}
-    d = {"F": "F[+F]F[-F]F"}
-    d = {"F": "F[+F]F[-F][F]"}
-    d = {"F": "F[+F]F[-F]F"}
-    return ''.join([d.get(c) or c for c in s])
+    new = ""
+    for c in s:
+        if c == "X":
+            new += "F[+X][-X]FX"
+        elif c == "F":
+            new += "FF"
+        else:
+            new += c
+    return new
+
+# def lSysCompute(s):
+#     d = {"F": "F[+F]F[-F]F"}
+#     d = {"F": "F[+F]F[-F][F]"}
+#     d = {"F": "F[+F]F[-F]F"}
+#     return ''.join([d.get(c) or c for c in s])
 
 def draw(t, s, length, angle):
     stack = deque()
     for c in s:
         if c in string.ascii_letters:
             t.forward(length)
+            # if c == "X":
+            #     t.penup()
+            #     t.forward(length)
+            #     t.pendown()
+            # elif c == "F":
+            #     t.forward(length)
         elif c == '-':
             t.left(angle)
         elif c == '+':
@@ -45,17 +61,30 @@ def main():
     t.left(90)
     t.pensize(1)
     t.penup()
-    t.setpos(0, -390)
+    t.setpos(0, -300)
     t.pendown()
     t.speed(0)
 
-    axiom = 'F'
-    length = 3.8
+    axiom = 'X'
+    length = 1
     angle = 25.7
-    iterations = 5
+    iterations = 7
 
-    draw(t, lSysGenerate(axiom, iterations), length, angle)
+
+    s = lSysGenerate(axiom, iterations)
+    # print(s)
+    draw(t, s, length, angle)
 
     wn.exitonclick()
 
 main()
+# new = ""
+# for c in "X[+F][-F]XF":
+#     if c == "X":
+#         new += "X[+F][-F]XF"
+#     elif c == "F":
+#         new += "FF"
+#     else:
+#         new += c
+
+# print(new)
