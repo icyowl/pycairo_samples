4082d417fbb75e2f124ccd66ae829f87e8c398ec
AkiraMac <frogcherry7@gmail.com>
2023-06-16 19:24:34 +0900

    tree

--- pycairo-keyaki.py
+++ pycairo-keyaki.py
@@ -11,56 +11,63 @@ WIDTH, HEIGHT = 512, 512
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
 
-theta, x, y = 90, 256, 480
+theta, w = 90, 5
+x, y = 256, 480
 stack = []
 
 def start():
     ctx.move_to(x, y)
 
 def forward(length):
-    global theta, x, y
+    global theta, w, x, y
     rad = math.radians(theta)
-    _x, _y = x, y
+    pre_x, pre_y = x, y
     x += math.cos(rad) * length
     y -= math.sin(rad) * length
-    print(_x, _y, x, y)
-    # ctx.line_to(x, y)
-    # ctx.stroke()
-    # ctx.move_to(x, y)
-    breadth = 5
-    r = math.radians(90-angle)
-    x0 = _x + math.cos(r) * breadth 
-    y0 = _y + math.sin(r) * breadth    
-    x1 = _x - math.cos(r) * breadth 
-    y1 = _y - math.sin(r) * breadth    
-    x2 = x - math.cos(r) * breadth 
-    y2 = y - math.sin(r) * breadth    
-    x3 = x + math.cos(r) * breadth 
-    y3 = y + math.sin(r) * breadth    
+    # print(x0, y0, x, y)
+
+    rad = math.radians(90-theta)
+    x0 = pre_x + math.cos(rad) * w 
+    y0 = pre_y + math.sin(rad) * w    
+    x1 = pre_x - math.cos(rad) * w 
+    y1 = pre_y - math.sin(rad) * w    
+    x2 = x - math.cos(rad) * w 
+    y2 = y - math.sin(rad) * w    
+    x3 = x + math.cos(rad) * w 
+    y3 = y + math.sin(rad) * w    
+
     ctx.move_to(x0, y0)
     ctx.line_to(x1, y1)
     ctx.line_to(x2, y2)
     ctx.line_to(x3, y3)
     ctx.line_to(x0, y0)
+    # ctx.close_path()
+    # ctx.set_source_rgba(0, 0, 0, 1)
+    ctx.fill_preserve()
     ctx.stroke()
     ctx.move_to(x, y)
 
 def left(length, angle):
-    global theta
+    global theta, w, x
     theta += angle
+    w = w / 2
+    x = x - w
     forward(length)
 
 def right(length, angle):
-    global theta
+    global theta, w, x
     theta -= angle
+    w = w / 2
+    x = x + w
     forward(length)
 
 def append():
-    stack.append((theta, (x, y)))
+    global theta, w, x, y
+    stack.append((theta, w, (x, y)))
 
 def pop():
-    global theta, x, y
-    theta, (x, y) = stack.pop()
+    global theta, w, x, y
+    theta, w, (x, y) = stack.pop()
     ctx.move_to(x, y)
 
 def draw(s, length, angle):
@@ -83,7 +90,8 @@ def tkview(surface):
     root = tk.Tk()
     root.title("L-system")
     w, h = surface.get_width(), surface.get_height()
-    img = Image.frombuffer("RGBA", (w, h), surface.get_data(), "raw", "RGBA", 0, 1)
+    buf = surface.get_data()
+    img = Image.frombuffer("RGBA", (w, h), buf, "raw", "RGBA", 0, 1)
     image = ImageTk.PhotoImage(img)
     tk.Label(root, image=image).pack()
     root.mainloop()
@@ -97,9 +105,9 @@ def lSysGenerate(s, d, order):
 if __name__ == "__main__":
 
     axiom = "X"
-    length = 20
+    length = 5
     angle = 25
-    iterations = 2
+    iterations = 5
     rule = {
         "X": "F[+X]F[-X]+X",
         "F": "FF",
@@ -108,34 +116,34 @@ if __name__ == "__main__":
     surface = draw(s, length, angle)
 
     # length, angle = 100, 30
-    # breadth = 5
+    # w = 5
     # x, y = 256, 256
     # ctx.move_to(x, y)
     # rad = math.radians(angle)
-    # _x = x + math.cos(rad) * length 
-    # _y = x - math.sin(rad) * length
+    # x0 = x + math.cos(rad) * length 
+    # y0 = x - math.sin(rad) * length
     # # ctx.line_to(x, y)
     # # ctx.stroke()
-    # ctx.move_to(_x, _y)
+    # ctx.move_to(x0, y0)
     
     # ctx.set_source_rgb(1., 1., 0.)
     # rad = math.radians(90-angle)
-    # x0 = x + math.cos(rad) * breadth 
-    # y0 = y + math.sin(rad) * breadth    
+    # x0 = x + math.cos(rad) * w 
+    # y0 = y + math.sin(rad) * w    
     # ctx.move_to(x0, y0)
     # # ctx.stroke()
 
     # ctx.set_source_rgb(1., 0., 1.)
-    # x1 = x - math.cos(rad) * breadth 
-    # y1 = y - math.sin(rad) * breadth    
+    # x1 = x - math.cos(rad) * w 
+    # y1 = y - math.sin(rad) * w    
     # ctx.line_to(x1, y1)
 
-    # x2 = _x - math.cos(rad) * breadth 
-    # y2 = _y - math.sin(rad) * breadth    
+    # x2 = x0 - math.cos(rad) * w 
+    # y2 = y0 - math.sin(rad) * w    
     # ctx.line_to(x2, y2)
 
-    # x3 = _x + math.cos(rad) * breadth 
-    # y3 = _y + math.sin(rad) * breadth    
+    # x3 = x0 + math.cos(rad) * w 
+    # y3 = y0 + math.sin(rad) * w    
     # ctx.line_to(x3, y3)
     # ctx.line_to(x0, y0)
     # ctx.stroke()
