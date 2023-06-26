8ad26fd63e6db0a5407fe2e9dadeab0181889d16
AkiraMac <frogcherry7@gmail.com>
2023-06-26 15:00:45 +0900

    aaa

--- matplot-lsystem.py
+++ matplot-lsystem.py
@@ -1,92 +1,52 @@
+from collections import deque
 import math
 import matplotlib.pyplot as plt 
 import string
 
-# fig = plt.figure(figsize=(4, 12))
-# ax = fig.add_subplot(3, 1, 1)
-# ax2 = fig.add_subplot(3, 1, 2)
-# ax3 = fig.add_subplot(3, 1, 3)
-
-
-fig, ax = plt.subplots(figsize=(4, 4))
-ax.set_xlim([0, 512])
-ax.set_ylim([0, 512])
-ax.axis("off")
-
-theta, x, y = 90, 256, 0
-stack = []
-
-def forward(length):
-    global theta, x, y
-    rad = math.radians(theta)
-    dx = math.cos(rad) * length
-    dy = math.sin(rad) * length
-    ax.arrow(x, y, dx, dy)
-    x += dx
-    y += dy
-
-def left(length, angle):
-    global theta
-    theta += angle
-    forward(length)
-
-def right(length, angle):
-    global theta
-    theta -= angle
-    forward(length)
-
-def append():
-    global theta, x, y
-    stack.append((theta, (x, y)))
-
-def pop():
-    global theta, x, y
-    theta, (x, y) = stack.pop()
-
-def draw(s, length, angle):
-    for c in s:
-        if c in string.ascii_letters:
-            forward(length)
-        elif c == '-':
-            left(length, angle)
-        elif c == '+':
-            right(length, angle)
-        elif c == "[":
-            append()
-        elif c == "]":
-            pop()
-
-def show(axiom, length, angle, iterations, rule):
-    plt.text(0, 150, f"axiom: {axiom}")
-    plt.text(0, 120, f"length: {length}")
-    plt.text(0, 90, f"θ: {angle}")
-    plt.text(0, 60, f"n: {iterations}")
-    for i, k in enumerate(rule.keys()):
-        txt = k + " → " + rule.get(k)
-        plt.text(0, 30 - i*30, txt)
-    plt.show()
-
-def lSysGenerate(s, d, order):
-    for i in range(order):
-        s = ''.join([d.get(c) or c for c in s])
+def lSystem(s, rule, iterations):
+    for _ in range(iterations):
+        s = ''.join([rule.get(c) or c for c in s])
 
     return s
 
+def plot(s, angle):
+    fig, ax = plt.subplots(figsize=(4,4))
+    ax.set_aspect("equal")
+    ax.axis("off")
+    color = "k"
+    theta = 0
+    x, y = 0, 0
+    stack = deque()
+    for i, c in enumerate(s):
+        if c in string.ascii_letters:
+            rad = math.radians(theta)
+            dx = math.cos(rad)
+            dy = math.sin(rad)
+            ax.plot((x, (x+dx)), (y, (y+dy)), c=color)
+            x += dx 
+            y += dy
+        elif c == "-":
+            theta += angle
+        elif c == "+":
+            theta -= angle
+        elif c == "[":
+            stack.append((theta, x, y))
+        elif c == "]":
+            theta, x, y = stack.pop()
+
+    plt.show()
+
 if __name__ == "__main__":
 
-    axiom = "X"
-    length = 5
-    angle = 25.7
-    iterations = 5
+    axiom = "F-F-F-F"
+    angle = 90py
+    iterations = 4
     rule = {
-        # "X": "F[+X]F[-X]+X",
-        # "F": "FF",
-        "X": "F[+X]-X",
-        "F": "FF"
+        # "F": "F+F-F-F+F",
+        "F": "FF-F-F-F-FF"
     }
-    s = lSysGenerate(axiom, rule, iterations)
-    draw(s, length, angle)
-    show(axiom, length, angle, iterations, rule)
+    s = lSystem(axiom, rule, iterations)
+    plot(s, angle)
 
 
 
