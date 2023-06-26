86c5abea189529b229162bbe5b3f44976f882501
AkiraWin <frog7bar@gmail.com>
2023-06-27 01:07:30 +0900

    add koch snow

--- matplot-lsystem.py
+++ matplot-lsystem.py
@@ -3,17 +3,18 @@ import math
 import matplotlib.pyplot as plt 
 import string
 
-def lSystem(s, rule, iterations):
-    for _ in range(iterations):
+def lSystem(s, rule, n):
+    for _ in range(n):
         s = ''.join([rule.get(c) or c for c in s])
 
     return s
 
 def plot(s, angle):
-    fig, ax = plt.subplots(figsize=(4,4))
+    fig = plt.figure(figsize=(4,4), facecolor="white")
+    ax = fig.add_subplot()
     ax.set_aspect("equal")
     ax.axis("off")
-    color = "k"
+    color = "black"
     theta = 0
     x, y = 0, 0
     stack = deque()
@@ -25,9 +26,9 @@ def plot(s, angle):
             ax.plot((x, (x+dx)), (y, (y+dy)), c=color)
             x += dx 
             y += dy
-        elif c == "-":
-            theta += angle
         elif c == "+":
+            theta += angle
+        elif c == "-":
             theta -= angle
         elif c == "[":
             stack.append((theta, x, y))
@@ -38,14 +39,15 @@ def plot(s, angle):
 
 if __name__ == "__main__":
 
-    axiom = "F-F-F-F"
-    angle = 90py
-    iterations = 4
+    axiom = "F--F--F"
+    # axiom = "F-F-F-F"
+    angle = 60
+    n = 4
     rule = {
-        # "F": "F+F-F-F+F",
-        "F": "FF-F-F-F-FF"
+        "F": "F+F--F+F",
+        # "F": "FF-F-F-F-FF"
     }
-    s = lSystem(axiom, rule, iterations)
+    s = lSystem(axiom, rule, n)
     plot(s, angle)
 
 
--- matplot-lsystem2.py
+++ matplot-lsystem2.py
@@ -1,62 +1,50 @@
-import math
 import matplotlib.pyplot as plt 
+import numpy as np
 import string
 
-def lSystem(s, rule, iterations):
-    for _ in range(iterations):
+def lsystem(s, rule, n):
+    for _ in range(n):
         s = ''.join([rule.get(c) or c for c in s])
 
     return s
 
-def plot(s, length, angle, alpha=1, colors={}):
-    fig, ax = plt.subplots(figsize=(4,4))
+def rotate(vec, angle):
+    rad = np.radians(angle)
+    mat = [[np.cos(rad), -np.sin(rad)], [np.sin(rad), np.cos(rad)]]
+    return np.dot(mat, vec)
+
+def plot(s, angle):
+    fig = plt.figure(figsize=(4,4), facecolor="white")
+    ax = fig.add_subplot()
     ax.set_aspect("equal")
     ax.axis("off")
-    color = "k"
-    theta = 90
-    x, y = 0, 0
+    pos = np.zeros(2, dtype=np.float64)
+    vec = np.array([1., 0])
     stack = []
-    for i, c in enumerate(s):
-        if i == 0:
-            length *= 2
-        if i == 1:
-            length /= 2
-        # if c in string.ascii_letters:
-        if c in string.ascii_uppercase:
-            rad = math.radians(theta)
-            dx = math.cos(rad) * length
-            dy = math.sin(rad) * length
-            ax.plot((x, (x+dx)), (y, (y+dy)), c=color)
-            x += dx 
-            y += dy
-        elif c == "-":
-            theta += angle
+    for c in s:
+        if c in string.ascii_letters:
+            new_pos = pos + vec
+            ax.plot((pos[0], new_pos[0]), (pos[1], new_pos[1]), color="black")
+            pos = new_pos
         elif c == "+":
-            theta -= angle
-        elif c == "/":
-            length /= alpha
-        elif c == "*":
-            length *= alpha
+            vec = rotate(vec, angle)
+        elif c == "-":
+            vec = rotate(vec, -angle)
         elif c == "[":
-            stack.append((theta, x, y))
+            stack.append((pos, vec))
         elif c == "]":
-            theta, x, y = stack.pop()
-        elif c in colors:
-            color = colors[c]
+            pos, vec = stack.pop()
 
     plt.show()
 
 if __name__ == "__main__":
 
-    axiom = "X"
-    length = 10
-    angle = 20
-    iterations = 8
+    axiom = "F"
+    axiom = "F--F--F"
+    angle = 60
+    n = 4
     rule = {
-        # "X": "F[+X]F[-X]+X",
-        # "F": "FF"
-        "X": "F/[g+X]-gX*",
-        "g": "k"
+        "F": "F+F--F+F",
     }
-    s = lSystem(axiom, rule, iterations)
-    plot(s, length, angle, alpha=1.2, colors={"g": (0,0,0,0.6), "k": (0,0,0,0.9)})
+    s = lsystem(axiom, rule, n)
+    plot(s, angle)
