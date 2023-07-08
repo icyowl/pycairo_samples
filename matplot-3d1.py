7ea79184752d4a518584a019f77a8e80aa176aae
AkiraMac <frogcherry7@gmail.com>
2023-07-08 18:03:15 +0900

    kami lib

--- matplot-3d1.py
+++ matplot-3d1.py
@@ -1,3 +1,6 @@
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 import numpy as np
 import string
@@ -22,15 +25,15 @@ def axis_equal_3d(ax):
     ax.set_ylim(mid_y - rng, mid_y + rng)
     ax.set_zlim(mid_z - rng, mid_z + rng)
 
-def rotate_x(vec, angle, width):
+def rotate_x(vec, angle):
     rad = np.radians(angle)
     mat = [[1, 0, 0], [0, np.cos(rad), -np.sin(rad)], [0, np.sin(rad), np.cos(rad)]]
-    return np.dot(mat, vec), width / 1.6
+    return np.dot(mat, vec)
 
-def rotate_y(vec, angle, width):
+def rotate_y(vec, angle):
     rad = np.radians(angle)
     mat = [[np.cos(rad), 0, -np.sin(rad)], [0, 1, 0], [np.sin(rad), 0, np.cos(rad)]]
-    return np.dot(mat, vec), width / 1.6
+    return np.dot(mat, vec)
 
 def plot3d(s, angle, alpha=1, colors={}):
     fig = plt.figure(figsize=(4,4))
@@ -41,27 +44,33 @@ def plot3d(s, angle, alpha=1, colors={}):
     pos = np.zeros(3, dtype=np.float64)
     vec = np.array([0, 0, 1.])
     stack = []
+    t = 1
     for i, c in enumerate(s):
         theta = angle + (np.random.rand() - 0.5) * 12
         if i == 1:
-            vec = vec * 12
+            vec = rotate_x(vec, (np.random.rand() - 0.5) * 2)
+            vec = vec * 15
         if i == 2:
-            vec = vec / 12
+            vec = vec / 15
         if i < len(s)-1:
             if c in "+-&^" and s[i+1] == "k":
-                theta = 42 + (np.random.rand() - 0.5) * 6
+                theta = 27 + (np.random.rand() - 0.5) * 6
+                t = 2 + (np.random.rand() - 0.5) * 2
+                vec = vec * t
+            elif c == "k":
+                vec = vec / t
         if c in string.ascii_uppercase:
             new_pos = pos + vec
             ax.plot([pos[0], new_pos[0]], [pos[1], new_pos[1]], [pos[2], new_pos[2]], linewidth=width, c=color)
             pos = new_pos
         elif c == "+":
-            vec, width = rotate_x(vec, theta, width)
+            vec = rotate_x(vec, theta)
         elif c == "-":
-            vec, width = rotate_x(vec, -theta, width)
+            vec = rotate_x(vec, -theta)
         elif c == "&":
-            vec, width = rotate_y(vec, theta, width)
+            vec = rotate_y(vec, theta)
         elif c == "^":
-            vec, width = rotate_y(vec, -theta, width)
+            vec = rotate_y(vec, -theta)
         elif c == "/":
             vec = vec * alpha
         elif c == "*":
@@ -72,8 +81,12 @@ def plot3d(s, angle, alpha=1, colors={}):
             pos, vec, width = stack.pop()
         elif c in colors:
             color = colors[c]
+        if c in "+-&^":
+            width = width / 1.6
 
     ax.view_init(elev=20, azim=10)
+
+    return fig, ax
 
 if __name__ == "__main__":
     
@@ -82,13 +95,20 @@ if __name__ == "__main__":
     angle = 27.5
     n = 5
     rule = {
-        "P1": "gF/[+X][-X][&X][^X]^X*",
-        "P2": "gF/[+X][-X][&X]^X*",
-        "P3": "gF/[+X][-X][&X]*",
-        # "X": "gF/[+X][-X][&X]^X*",
+        "P1": "gF/[+X][-X][^X]&X*",
+        "P2": "gF/[+X][-X][&X]*",
+        "P3": "gF/[+X][-X][^X]*",
         "g": "k"
     }
     s = lSystem(axiom, rule, n)
-    plot3d(s, angle, alpha=1.3, colors={"g": (0,0,0,0.6), "k": (0,0,0,0.9)})
+    fig, ax = plot3d(s, angle, alpha=1.4, colors={"g": (0,0,0,0.6), "k": (0,0,0,0.9)})
 
-    plt.show()
+    axis_equal_3d(ax)
+
+    plt.savefig("tree1.svg")
+
+    root = tk.Tk()
+    canvas = FigureCanvasTkAgg(fig, root)
+    canvas.get_tk_widget().pack()
+    plt.close()
+    root.mainloop()
